8600bc957db935720cca2aef9990bbbb8ac13120
Nugget102 <151664578+Nugget102@users.noreply.github.com>
2025-10-09 02:18:07 +0100

    Create CustomerNotifier.java

---
+++ src/main/java/com/cafepos/observer/CustomerNotifier.java
@@ -0,0 +1,12 @@
+package com.cafepos.observer;
+
+import com.cafepos.domain.Order;
+import com.cafepos.domain.OrderObserver;
+
+public final class CustomerNotifier implements OrderObserver {
+    @Override
+    public void updated(Order order, String eventType) {
+        System.out.println("[Customer] Dear customer, your Order #"
+                + order.id() + " has been updated: " + eventType + ".");
+    }
+}
