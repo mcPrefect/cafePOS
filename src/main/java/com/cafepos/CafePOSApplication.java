5e568f80938ae5b377744be4c57f54786deeb93b
mcPrefect <michael04cronin@gmail.com>
2025-10-12 17:26:26 +0100

    added vaadin ui

---
+++ src/main/java/com/cafepos/CafePOSApplication.java
@@ -0,0 +1,11 @@
+package com.cafepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafePOSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CafePOSApplication.class, args);
+    }
+}
